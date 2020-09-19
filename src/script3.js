747c53947ec0475a9a7406f1e17067c0c0aa55b3
Reshma <37250413+reshmaharidhas@users.noreply.github.com>
2020-09-20 00:29:41 +0530

    Create script3.js

---
+++ src/script3.js
@@ -0,0 +1,3 @@
+document.getElementById("foot01").innerHTML="<p>&copy;"+new Date().getFullYear()+"All Rights reserved.</p>";
+document.getElementById("nav03").innerHTML="<ul id='menu'><form action='#' id='f1'>"+"<li><input type='radio' name='status' value='yes'>Avail</li>"+
+"<li><input type='radio' name='status' value='no'>N/A</li>"+"<li><input type='submit' name='submit' value='Change'></li>"+"</form></ul>";
