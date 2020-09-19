153cae280e2434aec0de20bb895ca3ea76ebc516
Reshma <37250413+reshmaharidhas@users.noreply.github.com>
2020-09-19 16:20:26 +0530

    Create script.js
    Javascript file

---
+++ src/script.js
@@ -0,0 +1,2 @@
+document.getElementById("foot01").innerHTML="<p>&copy;"+new Date().getFullYear()+"All Rights reserved.</p>";
+document.getElementById("nav01").innerHTML="<ul id='menu'>"+"<li><a href='home.php'>Home</a></li>"+"<li><a href='about.html'>About</a></li>"+"</ul>";
