609d4c2d35139c715371fd795a2e3d06ac46ca7d
Reshma <37250413+reshmaharidhas@users.noreply.github.com>
2020-09-20 00:23:49 +0530

    Create page2.php

---
+++ src/page2.php
@@ -0,0 +1,18 @@
+<?php
+	session_start();
+	session_destroy();
+?>
+<html>
+<head>
+<title>Basic Login Script</title>
+<link href="site.css" rel="stylesheet">
+</head>
+<body>
+<nav id="nav01"></nav>
+<div id="main">
+User Logged Out</div>
+<footer id="foot01"></footer>
+</div>
+<script src="script.js"></script>
+</body>
+</html>
